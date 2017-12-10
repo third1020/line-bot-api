d3cff5471a79443ec607d1c1b3031207f18169ee
thirdppp <third1025@hotmail.com>
2017-12-11 00:09:25 +0700

    s

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('line-bot-api/php/line-bot.php');
+include ('/php/line-bot.php');
 
 $channelSecret = '8536dfaeea8d39c38ecffc995af9df0e';
 $access_token  = 'Ld+eM6OFnzB/FXXkdnWzzA5BQzav1KxAOM93agQmcy7cffx9RhyFgIOmnSUz5p9guUqM/A8FZqz5E/GZtROxJ04juzohuesrrkICIspKJjtOM8z09724+NViU9wr4CprSP3y0zMS7JoNexGQhi8ntQdB04t89/1O/w1cDnyilFU=
